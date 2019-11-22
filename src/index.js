b9dcf992fadda91aff1279c403ea4dac0ad8dd50
= <=>
2019-11-21 23:58:02 -0300

    chore: Styled header component implemented as react navigation.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import NavigationService from './services/navigation';
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="#191920" />
+      <Routes
+        ref={navigationRef => NavigationService.setNavigator(navigationRef)}
+      />
     </>
   );
 }
