76150fdd12dcd0f80c8d394f2e57ed294acf918e
= <=>
2019-11-24 17:59:11 -0300

    console tron on header to debug navigation.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,15 @@ import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
 
 // eslint-disable-next-line react/prop-types
 function Header({ navigation }) {
+  console.tron.log('NAVIGATION', navigation);
+
   return (
     <Wrapper>
       <Container>
         <Logo />
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#FFF" size={24} />
-          <ItemCount>3</ItemCount>
+          <ItemCount>2</ItemCount>
         </BasketContainer>
       </Container>
     </Wrapper>
