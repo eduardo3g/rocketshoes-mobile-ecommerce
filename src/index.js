1c59de51c9a34940464f74268831ca628afbf5a1
= <=>
2019-11-27 12:07:40 -0300

    chore: Initial Redux configuration.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
+import store from './store';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -7,11 +9,11 @@ import NavigationService from './services/navigation';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
       <Routes
         ref={navigationRef => NavigationService.setNavigator(navigationRef)}
       />
-    </>
+    </Provider>
   );
 }
