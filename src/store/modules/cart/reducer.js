729ac15fe4d830f7fb45b3cc7ddfd96f11fd40b0
= <=>
2019-11-27 16:01:19 -0300

    chore: Stock validation before add product to cart.

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,19 +4,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.item.id);
+        const { item } = action;
 
-        // If product exists
-        if (productIndex >= 0) {
-          // Add one more product on that product, instead of duplicating it on the list
-          draft[productIndex].amount += 1;
-        } else {
-          // If item does not exist, add it to products array.
-          draft.push({
-            ...action.item,
-            amount: 1,
-          });
-        }
+        draft.push(item);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
