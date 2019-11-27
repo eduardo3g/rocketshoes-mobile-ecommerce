8d2178f6f99e17ba4b85d42770c46d0965feb020
= <=>
2019-11-27 14:07:49 -0300

    fix: Duplicated items in the cart. When the user adds an item to cart, only the amount changes (+= 1) if it has already been added before.

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
       }
     ],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
     'no-console': ["error", { allow: ["tron"] }]
   },
 };
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,23 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.item,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.item.id);
+
+        // If product exists
+        if (productIndex >= 0) {
+          // Add one more product on that product, instead of duplicating it on the list
+          draft[productIndex].amount += 1;
+        } else {
+          // If item does not exist, add it to products array.
+          draft.push({
+            ...action.item,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
