8940a5af85c9f9c1ed637a6e7b40e78af34d84d6
= <=>
2019-11-27 11:39:52 -0300

    chore: Estilização do carrinho de compras.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,75 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Products,
+  Product,
+  ProductInfos,
+  ProductImage,
+  ProductDetailed,
+  ProductTitle,
+  ProductPrice,
+  ProductDeletion,
+  ProductIncrementDecrement,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+} from './styles';
+import colors from '../../styles/colors';
 
 export default function Cart() {
-  return <View />;
+  return (
+    <Container>
+      <Products>
+        <Product>
+          <ProductInfos>
+            <ProductImage
+              source={{
+                uri:
+                  'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+              }}
+            />
+            <ProductDetailed>
+              <ProductTitle>Tênis de caminhada leve</ProductTitle>
+              <ProductPrice>R$ 199,90</ProductPrice>
+            </ProductDetailed>
+            <ProductDeletion>
+              <Icon name="delete-forever" size={24} color={colors.primary} />
+            </ProductDeletion>
+          </ProductInfos>
+          <ProductIncrementDecrement>
+            <ProductControlButton>
+              <Icon
+                name="remove-circle-outline"
+                size={20}
+                color={colors.primary}
+              />
+            </ProductControlButton>
+            <ProductAmount value="2" />
+            <ProductControlButton>
+              <Icon
+                name="add-circle-outline"
+                size={20}
+                color={colors.primary}
+              />
+            </ProductControlButton>
+            <ProductSubtotal>R$ 1.250,00</ProductSubtotal>
+          </ProductIncrementDecrement>
+        </Product>
+      </Products>
+      <TotalContainer>
+        <TotalText>TOTAL</TotalText>
+        <TotalAmount>R$ 1250,00</TotalAmount>
+        <Order>
+          <OrderText>FINALIZAR PEDIDO</OrderText>
+        </Order>
+      </TotalContainer>
+    </Container>
+  );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  padding: 20px 10px;
+  background: #fff;
+  border-radius: 4px;
+  margin: 15px;
+`;
+
+export const Products = styled.View``;
+export const Product = styled.View``;
+
+export const ProductInfos = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const ProductImage = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+export const ProductDetailed = styled.View`
+  flex: 1;
+  margin-left: 15px;
+  padding: 10px;
+`;
+export const ProductTitle = styled.Text`
+  font-size: 15px;
+`;
+export const ProductPrice = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 3px;
+`;
+export const ProductDeletion = styled.TouchableOpacity`
+  padding: 6px;
+`;
+export const ProductIncrementDecrement = styled.View`
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  background-color: #eee;
+  padding: 8px;
+`;
+export const ProductControlButton = styled.TouchableOpacity``;
+
+export const ProductAmount = styled.TextInput.attrs({
+  readonly: true,
+})`
+  background: #fff;
+  padding: 5px;
+  border-radius: 4px;
+  margin: 0 5px;
+  border: 1px solid #ddd;
+  min-width: 50px;
+  text-align: center;
+`;
+export const ProductSubtotal = styled.Text`
+  flex: 1;
+  font-weight: bold;
+  font-size: 16px;
+  text-align: right;
+`;
+export const TotalContainer = styled.View`
+  margin-top: 30px;
+`;
+export const TotalText = styled.Text`
+  font-weight: bold;
+  text-align: center;
+  color: #999;
+`;
+export const Order = styled.TouchableOpacity`
+  background: ${colors.primary};
+  padding: 12px;
+  border-radius: 4px;
+`;
+export const OrderText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 14px;
+  text-align: center;
+`;
+export const TotalAmount = styled.Text`
+  font-weight: bold;
+  text-align: center;
+  font-size: 32px;
+  margin-top: 5px;
+  margin-bottom: 30px;
+`;
